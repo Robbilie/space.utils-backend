cdf261fd3442c300785b1255f0c82ea3216a06cf
Robert Schuh <gitlab@eneticum.de>
2016-08-14 19:54:42 +0200

    first steps for routes

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,17 +2,19 @@
 	"use strict";
 
 	const {Router} 					= require("express");
+	const passport 					= require("passport");
+	const login 					= require("connect-ensure-login");
 	//const DBUtil 					= require("util/DBUtil");
 	const RESTUtil 					= require("util/RESTUtil");
 
 	const m 	= { mergeParams: true };
 
 	module.exports = Router(m)
-		.get("/", () => {})
-		.get("/login", () => {})
-		.post("/login", () => {})
-		.get("/logout", () => {})
-		.get("/account", () => {})
+		.get("/", (req, res) => !req.query.code ? res.render("index") : res.render("index-with-code"))
+		.get("/login", (req, res) => res.render("login"))
+		.post("/login", [passport.authenticate("local", { successReturnToOrRedirect: "/", failureRedirect: "/login" })])
+		.get("/logout", (req, res) => { req.logout(); req.redirect("/"); })
+		.get("/account", [login.ensureLoggedIn(), (req, res) => res.render("account", { user: req.user })])
 		.get("/dialog/authorize", () => {})
 		.post("/dialog/authorize/decision", () => {})
 		.post("/oauth/token", () => {})
