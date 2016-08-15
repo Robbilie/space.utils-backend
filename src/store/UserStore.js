9785f64fa3a13f3240f34b848bc4f3e5540f6a1b
Robert Schuh <gitlab@eneticum.de>
2016-08-15 03:07:36 +0200

    fix

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -7,37 +7,36 @@
 	class UserStore extends Store {
 
 		aggregate (data) {
-			return this.collection
-				.aggregate([
-					{
-						$match: data
-					},
-					{
-						$unwind: {
-							path: "$characters",
-							preserveNullAndEmptyArrays: true
-						}
-					},
-					{
-						$lookup: {
-							from: "characters",
-							localField: "characters",
-							foreignField: "_id",
-							as: "characters"
-						}
-					},
-					{
-						$unwind: "$characters"
-					},
-					{
-						$group: {
-							_id: "$_id",
-							name: "$name",
-							password: "$password",
-							characters: { $push: "$characters" }
-						}
+			return this.collection.aggregate([
+				{
+					$match: data
+				},
+				{
+					$unwind: {
+						path: "$characters",
+						preserveNullAndEmptyArrays: true
 					}
-				]);
+				},
+				{
+					$lookup: {
+						from: "characters",
+						localField: "characters",
+						foreignField: "_id",
+						as: "characters"
+					}
+				},
+				{
+					$unwind: "$characters"
+				},
+				{
+					$group: {
+						_id: "$_id",
+						name: { $first: "$name" },
+						password: { $first: "$password" },
+						characters: { $push: "$characters" }
+					}
+				}
+			]).toArray().then(docs => docs.map(doc => new this.type(doc)));
 		}
 
 		getByName () {}
