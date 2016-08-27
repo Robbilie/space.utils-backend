04ad79cf447d1b7cd45a1c147fc028bc0b384871
Robert Schuh <gitlab@eneticum.de>
2016-08-28 00:16:04 +0200

    readd heapdump

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 	process.env.NODE_PATH = __dirname + "/src";
 	require("module").Module._initPaths();
 
+	const heapdump = require("heapdump");
+
 	require("lib/extendings")();
 
 	const LoadUtil = require("util/LoadUtil");
@@ -13,13 +15,9 @@
 	const App = LoadUtil.app(process.argv[2]);
 	const app = new App();
 
-	const DBUtil = require("util/DBUtil");
-
-
-
+	/* REPL */
 
 	const repl = require("repl");
-
 	const r = repl.start({
 		prompt: 'Node.js via stdin> ',
 		input: process.stdin,
@@ -28,19 +26,3 @@
 		replMode: repl.REPL_MODE_SLOPPY
 	});
 	r.context.app = app;
-
-
-	/*
-
-	DBUtil.getOplogCursor().then(cursor => {
-		const stream = cursor.stream();
-			stream.on("data", d => console.log(d));
-	});
-
-	DBUtil.getStore("Character").then(store => store.getUpdates()).then(cursor => {
-		let stream = cursor.stream();
-			stream.on("data", d => console.log("char:", d));
-
-	}).catch(e => console.log(e));
-
-	*/
