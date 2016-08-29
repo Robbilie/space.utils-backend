e8509a79a9f894bcdf6af95f091d362968bce4bb
Robert Schuh <gitlab@eneticum.de>
2016-08-29 06:42:32 +0200

    test

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,10 @@
 
 	"use strict";
 
-	module.exports = new Proxy({}, { get: (P,key) => require(`./${key}`) });
+	module.exports = new Proxy({}, {
+		get: (P, key) => {
+			const r = require(`./${key}`);
+			console.log(P, key, r);
+			return r;
+		}
+	});
