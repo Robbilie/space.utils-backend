1278b9f9def753a810ca12c55ee9fbce95ae9108
Robert Schuh <gitlab@eneticum.de>
2016-08-29 07:04:03 +0200

    does this work?

---
+++ src/scheme/BaseScheme.js
@@ -0,0 +1,11 @@
+	
+	"use strict";
+	
+	const { ObjectId } = require("mongodb");
+
+	module.exports = {
+		types: {
+			_id: 					ObjectId
+		},
+		aggregations: []
+	};
---
+++ src/scheme/EntityScheme.js
@@ -0,0 +1,10 @@
+
+	"use strict";
+
+	module.exports = {
+		types: {
+			id: 					Number,
+			name: 					String
+		},
+		aggregations: []
+	};
