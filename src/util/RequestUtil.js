8b555ebe544af3a7ef16a5ed083a709b557e7aba
Robert Schuh <gitlab@eneticum.de>
2016-09-09 18:08:54 +0200

    we are only interested in insert atm

--- src/util/RequestUtil.js
+++ src/util/RequestUtil.js
@@ -23,16 +23,16 @@
 				const requests = await DBUtil.getCollection("requests");
 				const responses = await DBUtil.getCollection("responses");
 
-				let cursor = await DBUtil.getOplogCursor({ ns: "responses" });
+				let cursor = await DBUtil.getOplogCursor({ ns: "responses", op: "i" });
 				let stream = cursor.stream();
 
 				stream.on("data", data => {
 
 					if(data.op == "i") {
 						if(storage.requests.get(data.o.id.toString())) {
+							responses.remove({ _id: data.o._id });
 							storage.requests.get(data.o.id.toString())(data.o.response);
 							storage.requests.delete(data.o.id.toString());
-							responses.remove({ _id: data.o._id });
 						}
 					}
 
