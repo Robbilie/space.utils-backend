1ec4e1b6106971fec0ca9a56a63da6a0fcb65ade
Robert Schuh <gitlab@eneticum.de>
2016-09-19 02:04:50 +0200

    debug dummy app

---
+++ src/app/DebugApp.js
@@ -0,0 +1,20 @@
+
+	"use strict";
+
+	class DebugApp {
+
+		constructor () {
+			try {
+				this.init();
+			} catch (e) {
+				console.log(e);
+			}
+		}
+
+		async init () {
+
+		}
+
+	}
+
+	module.exports = DebugApp;
