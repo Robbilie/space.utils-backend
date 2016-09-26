da92464709540a4d87b5ed5957655ace2c0884ee
Robert Schuh <gitlab@eneticum.de>
2016-09-26 22:44:17 +0200

    logging

--- src/handler/KillmailHandler.js
+++ src/handler/KillmailHandler.js
@@ -7,13 +7,17 @@
 
 		static filter () {
 			return async (req, res) => {
+				let d = Date.now();
 				console.log(req.body);
 				let store 			= await KillmailHandler.getStore();
+				console.log(Date.now() - d);
 				let killmails 		= await store.find(
 					KillmailHandler.sanitize(req.body.filter),
 					KillmailHandler.limit(req.body.options)
 				);
+				console.log(Date.now() - d);
 				res.json(await Promise.all(killmails.map(killmail => killmail.toJSON())));
+				console.log(Date.now() - d);
 			};
 		}
 
