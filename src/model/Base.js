bf47fb4a5212c8014e8828efa20d00c5188dde96
Robert Schuh <gitlab@eneticum.de>
2016-09-28 16:54:07 +0200

    faster?

--- src/model/Base.js
+++ src/model/Base.js
@@ -56,6 +56,7 @@
 
 		static toJSON (name, future, depth = 0) {
 			return new Promise(resolve => future.then(data => {
+				return resolve(data);
 
 				const result = data.constructor.name == "Object" ? {} : [];
 				const { types } = LoadUtil.scheme(name);
