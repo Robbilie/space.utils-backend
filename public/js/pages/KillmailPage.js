f0a59b2cca75ec656a514669ff6fba98c10d1d20
Robert Schuh <gitlab@eneticum.de>
2016-10-02 22:00:18 +0200

    killmail

--- public/js/pages/KillmailPage.js
+++ public/js/pages/KillmailPage.js
@@ -7,9 +7,27 @@
 			super(parent, "Killmail");
 			this.classList.push("page");
 
+			this.killID = killID - 0;
+
 			console.log("Kill", killID);
 
-			this.ready();
+			this.loadInitial().then(() => this.ready());
+		}
+
+		loadInitial () {
+			return fetch(`https://api.utils.space/killmails/${this.killID}`, {
+				method: "POST",
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ "filter": { killID: this.killID } })
+			}).then(res => res.json()).then(kill => {
+
+				console.log(kill);
+
+				return Promise.resolve();
+			});
 		}
 
 	}
