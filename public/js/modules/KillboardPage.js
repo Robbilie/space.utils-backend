16a6c4d90cce324d6d59d4b2c341b8d3fd1a262d
Robert Schuh <gitlab@eneticum.de>
2016-10-02 15:17:57 +0200

    append some kills

--- public/js/modules/KillboardPage.js
+++ public/js/modules/KillboardPage.js
@@ -6,7 +6,35 @@
 		constructor (parent) {
 			super(parent, "Killboard");
 			this.classList.push("page");
-			this.ready();
+
+			this.list = $(["div", { className: "killlist" }]);
+
+			this.loadInitial();
+		}
+
+		loadInitial () {
+			fetch("https://api.utils.space/killmails/", {
+				method: "POST",
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					"filter": { },
+					"options": { "sort": { "killID": -1 }, "limit": 50 }
+				})
+			}).then(res => res.json()).then(data => {
+				data.forEach(kill => this.getList().append($(["div", { innerHTML: JSON.stringify(kill) }])));
+				this.ready();
+			});
+		}
+
+		getList () {
+			return this.list;
+		}
+
+		render () {
+			return this.getList();
 		}
 
 	}
