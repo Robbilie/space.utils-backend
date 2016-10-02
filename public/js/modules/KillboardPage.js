3a78f39b35d6924684891f04ac873ca7ca3ee90e
Robert Schuh <gitlab@eneticum.de>
2016-10-02 17:41:29 +0200

    css

--- public/js/modules/KillboardPage.js
+++ public/js/modules/KillboardPage.js
@@ -9,10 +9,19 @@
 
 			this.list = $(["div", { className: "kill list" }]);
 
+			this.getList().on("scroll", () => {
+				if((this.getList().clientHeight + this.getList().scrollTop) / this.getList().scrollHeight * 100 > 80)
+					this.scrollDown();
+			});
+
 			this.loadInitial();
 		}
 
 		loadInitial () {
+			this.scrollDown(() => this.ready());
+		}
+
+		scrollDown (cb) {
 			fetch("https://api.utils.space/killmails/", {
 				method: "POST",
 				headers: {
@@ -20,16 +29,20 @@
 					'Content-Type': 'application/json'
 				},
 				body: JSON.stringify({
-					"filter": { },
+					"filter": Object.assign({}, this.lowKillID ? { killID: { $lt: this.lowKillID } } : {}),
 					"options": { "sort": { "killID": -1 }, "limit": 50 }
 				})
 			}).then(res => res.json()).then(data => {
-				data.forEach(kill => this.getList().append($(["a", { href: `/killmails/${kill.killID}/` }, [
-					$(["img", { src: `https://imageserver.eveonline.com/Type/${kill.victim.shipType.id}_64.png`, alt: kill.victim.shipType.name }]),
-					$(["span", { innerHTML: kill.victim.character.name }]),
-					$(["span", { innerHTML: (({ character, corporation, alliance, faction }, els = [character, corporation, alliance, faction]) => els.find(e => !!e))(kill.attackers.find(attacker => attacker.finalBlow)).name }])
-				]])));
-				this.ready();
+				data.forEach(kill => {
+					this.getList().append($(["a", { href: `/killmails/${kill.killID}/` }, [
+						$(["img", { src: `https://imageserver.eveonline.com/Type/${kill.victim.shipType.id}_64.png`, alt: kill.victim.shipType.name }]),
+						$(["span", { innerHTML: kill.victim.character.name }]),
+						$(["span", { innerHTML: (({ character, corporation, alliance, faction }, els = [character, corporation, alliance, faction]) => els.find(e => !!e))(kill.attackers.find(attacker => attacker.finalBlow)).name }])
+					]]));
+					this.lowKillID = Math.min(this.lowKillID || Number.MAX_VALUE, kill.killID);
+				});
+				if(cb)
+					cb();
 			});
 		}
 
