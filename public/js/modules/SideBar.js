72e7eadc7980f854e0b60a3e7b673f7380ec6499
Robert Schuh <gitlab@eneticum.de>
2016-10-03 18:32:46 +0200

    add home and live user update

--- public/js/modules/SideBar.js
+++ public/js/modules/SideBar.js
@@ -10,6 +10,7 @@
 				$(["span", { className: "sidebarOpen", innerHTML: "≡" }]),
 				$(["span", { className: "sidebarClose", innerHTML: "&times;" }])
 			]]);
+			this.usercounter = $(["div", { innerHTML: "TQ 26.364", className: "usercount" }]);
 		}
 
 		getToggle () {
@@ -20,9 +21,34 @@
 			return this.button;
 		}
 
+		getUserCounter () {
+			return this.usercounter;
+		}
+
+		updateUserCounter () {
+			let x = new XMLHttpRequest()
+				x.onload = e => {
+					let status = e.target.responseXML.getElementsByTagName("serverOpen")[0].innerHTML == "True";
+					let online = e.target.responseXML.getElementsByTagName("onlinePlayers")[0].innerHTML - 0;
+					let current = new Date(e.target.responseXML.getElementsByTagName("currentTime")[0].innerHTML + " GMT").getTime();
+					let cached = new Date(e.target.responseXML.getElementsByTagName("cachedUntil")[0].innerHTML + " GMT").getTime();
+
+					this.getUserCounter().innerHTML = "TQ " + online;
+					if(status)
+						this.getUserCounter().classList.add("online");
+					else
+						this.getUserCounter().classList.remove("online");
+
+					setTimeout(() => this.updateUserCounter(), cached - current);
+				};
+				x.open("GET", "https://api.eveonline.com/Server/ServerStatus.xml.aspx");
+				x.send();
+		}
+
 		render () {
 			return $(["div", { className: "sidebar" }, [
-				$(["div", { innerHTML: "TQ 26.364", className: "usercount" }])
+				this.getUserCounter(),
+				["a", { className: "homebtn", href: "/" }]
 			]]);
 		}
 
