aaadc57d981f6a94f9eadb2f8e6daa7ac17004e9
Robert Schuh <gitlab@eneticum.de>
2016-10-03 21:46:31 +0200

    test api performance

--- src/app/APIApp.js
+++ src/app/APIApp.js
@@ -26,7 +26,13 @@
 			web.set("json spaces", 2);
 			web.enable("trust proxy");
 
-			web.use(cors());
+			//web.use(cors());
+			web.use(function(req, res, next) {
+				res.header("Access-Control-Allow-Origin", "*");
+				res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+				res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+				next();
+			});
 			web.use(bodyParser.json());
 			web.use(bodyParser.urlencoded({ extended: false }));
 			web.use(cookieParser(config.cookies.secret));
