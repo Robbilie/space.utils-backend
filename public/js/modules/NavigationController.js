816e465504c6faeb9532257e7337d75fcd88daa4
Robert Schuh <gitlab@eneticum.de>
2016-10-03 04:36:26 +0200

    fix external links

--- public/js/modules/NavigationController.js
+++ public/js/modules/NavigationController.js
@@ -23,7 +23,7 @@
 
 			this.getApp().getParent().on("click", (e) => {
 				let link = e.path ? e.path.find(el => el.tagName == "A") : (e.target.tagName == "A" ? e.target : undefined);
-				if(!e.button && !e.ctrlKey && link) {
+				if(!e.button && !e.ctrlKey && link && link.href.split("://").length == 1) {
 					e.stopPropagation();
 					e.preventDefault();
 					this.navigate(link.href);
