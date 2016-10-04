93a23adb6a1fadff800a37a0e9008e23932ef70a
Robert Schuh <gitlab@eneticum.de>
2016-10-04 22:56:00 +0200

    dna

--- public/js/modules/Helper.js
+++ public/js/modules/Helper.js
@@ -12,10 +12,10 @@
 		static getGraphicIDs () {
 			if(!helperStorage.graphicIDs) {
 				let cgl = ccpwgl_int();
-				helperStorage.graphicIDs = Promise.all(
+				helperStorage.graphicIDs = Promise.all([
 					fetch(cgl.resMan.BuildUrl("res:/staticdata/typeids.json")).then(res => res.json()),
 					fetch(cgl.resMan.BuildUrl("res:/staticdata/graphicids.json")).then(res => res.json())
-				);
+				]);
 			}
 			return helperStorage.graphicIDs;
 		}
