189b8eb57dc2f819af7b23238c42f39692e961dc
Robert Schuh <gitlab@eneticum.de>
2016-10-04 15:12:06 +0200

    css

--- public/js/modules/PageController.js
+++ public/js/modules/PageController.js
@@ -49,6 +49,7 @@
 			instance
 				.onReady()
 				.then(() => instance.onInserted())
+				.wait(10)
 				.then(() => window.requestAnimationFrame(() => !window.getComputedStyle(element) || element.classList.remove("intransition") || this.getApp().setLoadingState(false) || console.log(Date.now() - d)));
 
 		}
