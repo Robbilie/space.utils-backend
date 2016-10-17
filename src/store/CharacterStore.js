e1375ed4417b00ddf590f2752d49f9b5c28ba359
Robert Schuh <gitlab@eneticum.de>
2016-10-17 17:44:05 +0200

    fix npc chars

--- src/store/CharacterStore.js
+++ src/store/CharacterStore.js
@@ -2,7 +2,10 @@
 	"use strict";
 
 	const { EntityStore } 			= require("store/");
-	const { CharacterInfoTask } 	= require("task/");
+	const {
+		CharacterInfoTask,
+		NPCAffiliationTask
+	} 	= require("task/");
 
 	class CharacterStore extends EntityStore {
 
@@ -15,9 +18,16 @@
 				if(await character.isNull()) {
 					await CharacterInfoTask.create({ characterID: id });
 					character = await this.findById(id);
-					if(await character.isNull()) {
-						console.log("MISSING CHAR", id);
-					}
+				}
+
+				if(await character.isNull()) {
+					await NPCAffiliationTask.create({ ids: [id] });
+					character = await this.findById(id);
+					console.log("NPC CHAR?", id);
+				}
+
+				if(await character.isNull()) {
+					console.log("MISSING CHAR", id);
 				}
 
 				return character;
