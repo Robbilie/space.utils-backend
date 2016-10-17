1fb3c827c11e6363cd66025f53f3c5c8d77c2304
Robert Schuh <gitlab@eneticum.de>
2016-10-17 17:00:24 +0200

    add faction

--- src/store/FactionStore.js
+++ src/store/FactionStore.js
@@ -2,16 +2,20 @@
 	"use strict";
 
 	const { EntityStore } 			= require("store/");
-	const { Faction } 				= require("model/");
+
 
 	class FactionStore extends EntityStore {
 
 		async findOrCreate (id, {} = $(1, { id }, "Number")) {
 			try {
 
-				console.log("MISSING FACTION", id);
+				let alliance = await this.findById(id);
 
-				return new Faction(Promise.resolve());
+				if(await alliance.isNull()) {
+					console.log("MISSING FACTION", id);
+				}
+
+				return alliance;
 
 			} catch (e) { console.log(e, new Error()); }
 		}
