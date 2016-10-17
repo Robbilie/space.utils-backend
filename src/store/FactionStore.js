310c5b89a1c9f7112a0cda35765c102588535229
Robert Schuh <gitlab@eneticum.de>
2016-10-17 16:56:45 +0200

    add faction

--- src/store/FactionStore.js
+++ src/store/FactionStore.js
@@ -5,7 +5,16 @@
 
 	class FactionStore extends EntityStore {
 
+		async findOrCreate (id, {} = $(1, { id }, "Number")) {
+			try {
+
+				console.log("MISSING FACTION", id);
+
+				return new Faction(Promise.resolve());
+
+			} catch (e) { console.log(e, new Error()); }
+		}
+
 	}
 
 	module.exports = FactionStore;
-	
