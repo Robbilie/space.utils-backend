3ff7c5becdd57b27ee83d80890198bf6e57788e4
Robert Schuh <gitlab@eneticum.de>
2016-10-19 14:50:18 +0200

    is this correct?

--- src/task/KillmailJsonTask.js
+++ src/task/KillmailJsonTask.js
@@ -21,13 +21,82 @@
 				try {
 
 					let killmailStore = await DBUtil.getStore("Killmail");
-					await killmailStore.update(
-						{ killID: response.killID },
-						{
-							$set: Object.assign(response, { hash: (await this.getData()).hash })
-						},
-						{ upsert: true }
-					);
+
+					let data = {
+						killID: response.killID,
+						hash: (await this.getData()).hash,
+						killTime: (new Date(response.killTime + " UTC")).getTime(),
+						attackerCount: response.attackerCount,
+						solarSystemID: response.solarSystem.id,
+						warID: response.war.id,
+						victim: {
+							damageTaken: response.victim.damageTaken
+						}
+					};
+
+					if(response.victim.position)
+						data.victim.position = reponse.victim.position;
+
+					if(response.victim.character)
+						data.victim.characterID = response.victim.character.id;
+
+					if(response.victim.corporation)
+						data.victim.corporationID = response.victim.corporation.id;
+
+					if(response.victim.alliance)
+						data.victim.allianceID = response.victim.alliance.id;
+
+					if(response.victim.faction)
+						data.victim.factionID = response.victim.faction.id;
+
+					if(response.victim.shipType)
+						data.victim.shipTypeID = response.victim.shipType.id;
+
+					data.victim.items = response.victim.items.map(item => {
+						let res = {
+							singleton: 		item.singleton,
+							flag: 			item.flag,
+							itemTypeID: 	item.itemType.id
+						};
+
+						if(item.quantityDropped)
+							res.quantityDropped = item.quantityDropped;
+
+						if(item.quantityDestroyed)
+							res.quantityDestroyed = item.quantityDestroyed;
+
+						return res;
+					});
+
+					data.attackers = response.attackers.map(attacker => {
+						let res = {
+							damageDone: 		attacker.damageDone,
+							finalBlow: 			attacker.finalBlow,
+							securityStatus: 	attacker.securityStatus
+						};
+
+						if(attacker.character)
+							res.characterID = attacker.character.id;
+
+						if(attacker.corporation)
+							res.corporationID = attacker.corporation.id;
+
+						if(attacker.alliance)
+							res.allianceID = attacker.alliance.id;
+
+						if(attacker.faction)
+							res.factionID = attacker.faction.id;
+
+						if(attacker.shipType)
+							res.shipTypeID = attacker.shipType.id;
+
+						if(attacker.weaponType)
+							res.weaponTypeID = attacker.weaponType.id;
+
+						return res;
+					});
+
+					await killmailStore.update({ killID: response.killID }, { $set: data }, { upsert: true });
 
 				} catch(e) { console.log(e, new Error()) }
 
