7b30c0cc1723da8790c2eaf49ce6b59fe1f59b19
Robert Schuh <gitlab@eneticum.de>
2016-11-02 22:00:27 +0100

    swagger start

--- src/handler/index.js
+++ src/handler/index.js
@@ -4,5 +4,5 @@
 	module.exports = new Proxy({}, { get: (P,key) => {
 		if(typeof(key) == "symbol")
 			throw new Error(key);
-		console.log(key) || require(`./${key}`);
+		return require(`./${key}`);
 	}});
