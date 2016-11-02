aebdb0e72c1575e9e843a8cbb0924edb8c57f677
Robert Schuh <gitlab@eneticum.de>
2016-11-02 22:14:02 +0100

    swagger start

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,7 +1,7 @@
 
 	"use strict";
 
-	module.exports = new Proxy({}, { get: (P,key) => {
+	/*module.exports = new Proxy({}, { get: (P,key) => {
 		console.log(key);
 		if(typeof(key) == "symbol")
 			return {};
@@ -10,4 +10,13 @@
 		} catch (e) {
 			return undefined;
 		}
-	}});
+	}});*/
+
+	module.exports = {
+		BaseHandler: require("./BaseHandler"),
+		EntityHandler: require("./EntityHandler"),
+		CharacterHandler: require("./CharacterHandler"),
+		CorporationHandler: require("./CorporationHandler"),
+		AllianceHandler: require("./AllianceHandler"),
+		KillmailHandler: require("./KillmailHandler")
+	};
