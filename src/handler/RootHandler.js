34dc70cbccd207f7cd78c98bf8e4624787401617
Robert Schuh <gitlab@eneticum.de>
2016-11-04 15:30:34 +0100

    start the template

--- src/handler/RootHandler.js
+++ src/handler/RootHandler.js
@@ -1,6 +1,14 @@
 
 	"use strict";
 
+	const ejs 				= require("ejs");
+	const fs 				= require("fs");
+	const jsyaml 			= require("js-yaml");
+	const client 			= ejs.render(
+		fs.readFileSync(process.env.NODE_PATH + "/views/client.ejs"),
+		jsyaml.safeLoad(fs.readFileSync(process.env.NODE_PATH + "/../routes/swagger.yaml")),
+		{}
+	);
 	const { BaseHandler } 	= require("handler/");
 	const config 			= require("util/../../config/");
 
@@ -18,6 +26,13 @@
 					"types"
 				].reduce((p, c) => !(p[c] = { href: `${config.site.url}/${c}/` }) || p, {})
 			);
+		}
+
+		static client () {
+			return async (req, res) => {
+				res.set("Content-Type", "text/javascript");
+				res.send(client);
+			};
 		}
 
 	}
