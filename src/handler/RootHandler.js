ba4388c46eea35273df23819d83f53d869ef540d
Robert Schuh <gitlab@eneticum.de>
2016-11-04 15:36:54 +0100

    start the template

--- src/handler/RootHandler.js
+++ src/handler/RootHandler.js
@@ -4,11 +4,7 @@
 	const ejs 				= require("ejs");
 	const fs 				= require("fs");
 	const jsyaml 			= require("js-yaml");
-	const client 			= ejs.render(
-		fs.readFileSync(process.env.NODE_PATH + "/../views/client.ejs"),
-		jsyaml.safeLoad(fs.readFileSync(process.env.NODE_PATH + "/../routes/swagger.yaml")),
-		{}
-	);
+	const swagger 			= jsyaml.safeLoad(fs.readFileSync(process.env.NODE_PATH + "/../routes/swagger.yaml"));
 	const { BaseHandler } 	= require("handler/");
 	const config 			= require("util/../../config/");
 
@@ -30,8 +26,10 @@
 
 		static client () {
 			return async (req, res) => {
-				res.set("Content-Type", "text/javascript");
-				res.send(client);
+				ejs.renderFile(process.env.NODE_PATH + "/../views/client.ejs", swagger, (err, data) => {
+					res.set("Content-Type", "text/javascript");
+					res.send(data);
+				});
 			};
 		}
 
