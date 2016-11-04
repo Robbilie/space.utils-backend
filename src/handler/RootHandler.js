32d654a3fc9a5b5f7b2737b69ed771cb1d1c2206
Robert Schuh <gitlab@eneticum.de>
2016-11-04 14:21:42 +0100

    add factions

--- src/handler/RootHandler.js
+++ src/handler/RootHandler.js
@@ -6,26 +6,17 @@
 	class RootHandler extends BaseHandler {
 
 		static home () {
-			return async (req, res) => res.json({
-				characters: {
-					href: `${config.site.url}/characters/`
-				},
-				corporations: {
-					href: `${config.site.url}/corporations/`
-				},
-				alliances: {
-					href: `${config.site.url}/alliances/`
-				},
-				killmails: {
-					href: `${config.site.url}/killmails/`
-				},
-				systems: {
-					href: `${config.site.url}/systems/`
-				},
-				types: {
-					href: `${config.site.url}/types/`
-				}
-			});
+			return async (req, res) => res.json(
+				[
+					"alliances",
+					"characters",
+					"corporations",
+					"factions",
+					"killmails",
+					"systems",
+					"types"
+				].reduce((p, c) => !(p[c] = { href: `${config.site.url}/${c}/` }) || p, {})
+			);
 		}
 
 	}
