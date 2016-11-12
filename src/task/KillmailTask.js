2679dd4e33600176aa81d7ff6d4d9b3e8729562a
Robert Schuh <gitlab@eneticum.de>
2016-11-12 02:39:08 +0100

    killmail task

---
+++ src/task/KillmailTask.js
@@ -0,0 +1,36 @@
+
+	"use strict";
+
+	const { BaseTask } 				= require("task/");
+	const { ESIUtil } 				= require("util/");
+
+	class KillmailTask extends BaseTask {
+
+		async start () {
+
+			let client = await ESIUtil.get_client();
+
+			let killmail_response = await client.Killmails.get_killmails_killmail_id_killmail_hash(this.get_data());
+
+			let killmail = killmail_response.obj;
+				delete killmail.killmail_id;
+				killmail.id 	= this.get_data().killmail_id;
+				killmail.hash 	= this.get_data().killmail_hash;
+
+			let killmails = await this.get_collection();
+
+			await killmails.update(
+				{ id: killmail.id },
+				{
+					$set: killmail
+				},
+				{ upsert: true }
+			);
+
+			await this.destroy();
+
+		}
+
+	}
+
+	module.exports = KillmailTask;
