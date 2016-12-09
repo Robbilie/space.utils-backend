5f511716ef8a085d63301080dad38dd2b67f4ac5
Robert Schuh <gitlab@eneticum.de>
2016-12-09 15:20:16 +0100

    perf?

--- src/util/ESIUtil.js
+++ src/util/ESIUtil.js
@@ -2,6 +2,15 @@
 	"use strict";
 
 	const Swagger = require("swagger-client");
+	const rp = require("request-promise-native");
+	const request = rp.defaults({
+		gzip: true,
+		forever: true,
+		timeout: 1000 * 10,
+		pool: {
+			maxSockets: Infinity
+		}
+	});
 
 	const storage = {
 		client: undefined
@@ -16,7 +25,27 @@
 		}
 
 		static new_client (options = {}) {
-			return new Swagger(Object.assign({ url: process.env.ESI_URL, usePromise: true, authorizations: { someHeaderAuth: new Swagger.ApiKeyAuthorization("User-Agent", process.env.UA, "header") } }, options));
+			return new Swagger(Object.assign({
+				url: process.env.ESI_URL,
+				usePromise: true,
+				authorizations: {
+					someHeaderAuth: new Swagger.ApiKeyAuthorization("User-Agent", process.env.UA, "header")
+				},
+				client: {
+					execute: async (obj) => {
+						try {
+							obj.on.response(await request({
+								method: obj.method,
+								url: obj.url,
+								headers: obj.headers,
+								body: obj.body
+							}));
+						} catch (e) {
+							obj.on.error(e);
+						}
+					}
+				}
+			}, options));
 		}
 		
 	}
