68f00fa1b80428784ced67c37885c3a6b0cfd283
Robert Schuh <gitlab@eneticum.de>
2017-01-11 15:14:23 +0100

    wat

--- src/util/ESIUtil.js
+++ src/util/ESIUtil.js
@@ -4,6 +4,66 @@
 	const Swagger 	= require("swagger-client");
 	const spdy 		= require("spdy");
 	const http2 	= require("http2");
+
+	http2.Agent.prototype.addRequest = function addRequest(req, options) {
+		// Legacy API: addRequest(req, host, port, localAddress)
+		if (typeof options === 'string') {
+			options = {
+				host: options,
+				port: arguments[2],
+				localAddress: arguments[3]
+			};
+		}
+
+		options = Object.assign({}, options);
+		options = Object.assign(options, this.options);
+
+		if (!options.servername) {
+			options.servername = options.host;
+			const hostHeader = req.getHeader('host');
+			if (hostHeader) {
+				options.servername = hostHeader.replace(/:.*$/, '');
+			}
+		}
+
+		var name = this.getName(options);
+		if (!this.sockets[name]) {
+			this.sockets[name] = [];
+		}
+
+		var freeLen = this.freeSockets[name] ? this.freeSockets[name].length : 0;
+		var sockLen = freeLen + this.sockets[name].length;
+
+		if (freeLen) {
+			// we have a free socket, so use that.
+			var socket = this.freeSockets[name].shift();
+
+			// don\'t leak
+			if (!this.freeSockets[name].length)
+				delete this.freeSockets[name];
+
+			socket.ref();
+			req.onSocket(socket);
+			this.sockets[name].push(socket);
+		} else if (sockLen < this.maxSockets) {
+			// If we are under maxSockets create a new one.
+			this.createSocket(req, options, function(err, newSocket) {
+				if (err) {
+					process.nextTick(function() {
+						req.emit('error', err);
+					});
+					return;
+				}
+				req.onSocket(newSocket);
+			});
+		} else {
+			// We are over limit so we\'ll add it to the queue.
+			if (!this.requests[name]) {
+				this.requests[name] = [];
+			}
+			this.requests[name].push(req);
+		}
+	};
 	const rp 		= require("request-promise-native");
 	const request 	= rp.defaults({
 		/*agentClass: spdy.Agent,
