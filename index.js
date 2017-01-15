01c33c4ed6dc305860133e31864cb3aa71460cab
Robert Schuh <gitlab@eneticum.de>
2017-01-15 18:20:20 +0100

    spam

--- index.js
+++ index.js
@@ -30,7 +30,8 @@
 		var oldCall = Function.prototype.call;
 		var newCall = function(self) {
 			Function.prototype.call = oldCall;
-			console.log('Function called:', this.name);
+			if(this.name != "toString")
+				console.log('call:', this.name);
 			var args = Array.prototype.slice.call(arguments, 1);
 			var res = this.apply(self, args);
 			Function.prototype.call = newCall;
