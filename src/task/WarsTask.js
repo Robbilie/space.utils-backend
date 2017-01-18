19f78fb0fcab7edb4f971082d0e70454fe3b265c
Robert Schuh <gitlab@eneticum.de>
2017-01-18 16:58:08 +0100

    hum

--- src/task/WarsTask.js
+++ src/task/WarsTask.js
@@ -18,7 +18,11 @@
 
 			const { expires, ids } = await ESIUtil.get_all_pages(client.Wars.get_wars);
 			console.log("wars", ids.length);
-			process.nextTick(() => ids.forEach(id => process.nextTick(() => WarStore.find_or_create(id))));
+
+			let chunks = ids.chunk(2000);
+			const process_chunk = chunk => new Promise(resolve => chunk.forEach(id => WarStore.find_or_create(id)) || resolve());
+			for (let i = 0; i < chunks.length; i++)
+				await process_chunk(chunks[i]);
 
 			await this.update({
 				expires
