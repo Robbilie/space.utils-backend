8565f0183816462bf9b63f72e6eceaad282e2f12
Robert Schuh <gitlab@eneticum.de>
2017-01-19 20:11:41 +0100

    style

--- public/js/page/HomePage.js
+++ public/js/page/HomePage.js
@@ -10,15 +10,15 @@
 					E("img", { src: "/img/1x2.png", className: "mobo" }),
 					E("img", { src: "/img/1x2.png", className: "mobo" }),
 					E("div", { className: "welcome-conti" },
-						E(Link, { id: "search", to: "/search/#search", style: "clip-path: url(#pone);" },
+						E(Link, { id: "search", to: "/search/#search", style: { clipPath: "url(#pone)" } },
 							E("h2", null, "SEARCH"),
 							E("img", { src: "/img/1x2.png" })
 						),
-						E(Link, { id: "killboard", to: "/killboard/#killboard", style: "clip-path: url(#ptwo);" },
+						E(Link, { id: "killboard", to: "/killboard/#killboard", style: { clipPath: "url(#ptwo)" } },
 							E("h2", null, "KILLBOARD"),
 							E("img", { src: "/img/1x2.png" })
 						),
-						E("a", { id: "service", href: "https://service.eneticum.de/", target: "_blank", style: "clip-path: url(#pthree);" },
+						E("a", { id: "service", href: "https://service.eneticum.de/", target: "_blank", style: { clipPath: "url(#pthree)" } },
 							E("h2",null, "SERVICE"),
 							E("img", { src: "/img/1x2.png" })
 						)
