58debddf126fe76b0b346468c452e9893965ff52
Robert Schuh <gitlab@eneticum.de>
2017-01-20 13:57:03 +0100

    some stuff

--- public/js/App.js
+++ public/js/App.js
@@ -3,13 +3,23 @@
 
 	class App extends Component {
 
+		constructor (props) {
+			super(props);
+			this.state = {
+				isSearching: false,
+				isLoading: false,
+				isOpen: false
+			};
+		}
+
 		render () {
 			return E("div", { className: "ui" },
-				E(TopBar),
-				E(SideBar),
+				E(SideBar, { isOpen: this.state.isOpen }),
 				E("div", { className: "content" },
 					this.props.children
-				)
+				),
+				E(TopBar, { isSearching: this.state.isSearching }),
+				E(Loading, { isLoading: this.state.isLoading })
 			);
 		}
 
---
+++ public/js/component/Loading.js
@@ -0,0 +1,10 @@
+
+	"use strict";
+
+	class Loading extends Component {
+
+		render () {
+			return E("div", { className: "loader" });
+		}
+
+	}
