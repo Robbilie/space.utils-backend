452cee507ff985f893d42f4eae4108a1f8b571c2
Robert Schuh <gitlab@eneticum.de>
2017-01-20 14:50:09 +0100

    user count

--- public/js/component/ServerStatus.js
+++ public/js/component/ServerStatus.js
@@ -3,8 +3,44 @@
 
 	class ServerStatus extends Component {
 
+		constructor (props) {
+			super(props);
+			this.state = {
+				status: "",
+				online: 0
+			};
+		}
+
+		componentDidMount () {
+			this.updateUserCounter();
+		}
+
+		componentWillUnmount () {
+			clearTimeout(this.timeout);
+		}
+
+		updateUserCounter () {
+			let x = new XMLHttpRequest();
+			x.onload = e => {
+				let status = e.target.responseXML.getElementsByTagName("serverOpen")[0].innerHTML == "True";
+				let online = e.target.responseXML.getElementsByTagName("onlinePlayers")[0].innerHTML - 0;
+				let current = new Date(e.target.responseXML.getElementsByTagName("currentTime")[0].innerHTML + " GMT").getTime();
+				let cached = new Date(e.target.responseXML.getElementsByTagName("cachedUntil")[0].innerHTML + " GMT").getTime();
+
+				this.setState({
+					status,
+					online
+				});
+
+				this.timeout = setTimeout(() => this.updateUserCounter(), cached - current);
+			};
+			x.onerror = e => setTimeout(() => this.updateUserCounter(), 1000);
+			x.open("GET", "https://api.eveonline.com/Server/ServerStatus.xml.aspx");
+			x.send();
+		}
+
 		render () {
-			return E("div", { className: "usercount" });
+			return E("div", { className: `usercount ${this.state.status ? "online" : ""}` }, E("span", null, "TQ"), " ", this.state.online.toLocaleString());
 		}
 
 	}
