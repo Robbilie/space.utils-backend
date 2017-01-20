fad4589d3ec4f65378d1e05e0a33f78fd6ca24f0
Robert Schuh <gitlab@eneticum.de>
2017-01-20 15:41:42 +0100

    swagger-client

--- public/js/base.js
+++ public/js/base.js
@@ -7,7 +7,10 @@
 
 	const ESIClient = new SwaggerClient({
 		url: "https://esi.tech.ccp.is/latest/swagger.json",
-		usePromise: true
+		usePromise: true,
+		authorizations: {
+			someHeaderAuth: new Swagger.ApiKeyAuthorization("User-Agent", "https://utils.space/", "header")
+		}
 	});
 	const EASClient = new SwaggerClient({
 		url: "https://api.utils.space/api-docs",
--- public/js/component/SearchBar.js
+++ public/js/component/SearchBar.js
@@ -5,8 +5,9 @@
 
 		search (search) {
 			ESIClient
-				.then(client => client.get_search({ search, categories: ["agent", "alliance", "character", "constellation", "corporation", "faction", "inventorytype", "region", "solarsystem", "station", "wormhole"] }))
-				.then(({ obj }) => console.log(obj));
+				.then(client => client.Search.get_search({ search, categories: ["agent", "alliance", "character", "constellation", "corporation", "faction", "inventorytype", "region", "solarsystem", "station", "wormhole"] }))
+				.then(({ obj }) => console.log(obj))
+				.catch(e => console.log("E", e));
 			/*
 			fetch(`https://esi.tech.ccp.is/latest/search/?search=${query}&categories=agent%2Calliance%2Ccharacter%2Cconstellation%2Ccorporation%2Cfaction%2Cinventorytype%2Cregion%2Csolarsystem%2Cstation%2Cwormhole&language=en-us&strict=false&datasource=tranquility`)
 				.then(res => res.json())
