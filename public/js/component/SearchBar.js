a6e9f498e75b4f7a5b5d2076cdb83861754a0acd
Robert Schuh <gitlab@eneticum.de>
2017-01-20 19:33:29 +0100

    hum

--- public/js/component/SearchBar.js
+++ public/js/component/SearchBar.js
@@ -5,8 +5,20 @@
 
 		search (search) {
 			ESIClient
-				.then(client => client.Search.get_search({ search, categories: ["alliance", "character", "corporation", "inventorytype", "solarsystem"/*, "region"*/] }))
-				.then(({ obj }) => console.log(obj))
+				.then(client => client.Search.get_search({ search, categories: ["alliance", "character", "corporation", "inventorytype", "solarsystem"/*, "region"*/] })
+					.then(({ obj: { alliance = [], character = [], corporation = [], inventorytype = [], solarsystem = [] } }) =>
+						client.Universe.post_universe_names({ ids: [...alliance, ...character, ...corporation, ...inventorytype, ...solarsystem] })
+					)
+					.then(entities => Object
+						.entries(entities
+							.reduce((p, { category, id, name }) => {
+								p[category] = { id, name }; return p;
+							}, {})
+						)
+						.sort(([a], [b]) => a > b ? 1 : -1)
+					)
+				)
+				.then(data => console.log(data))
 				.catch(e => console.log("E", e));
 			/*
 			fetch(`https://esi.tech.ccp.is/latest/search/?search=${query}&categories=agent%2Calliance%2Ccharacter%2Cconstellation%2Ccorporation%2Cfaction%2Cinventorytype%2Cregion%2Csolarsystem%2Cstation%2Cwormhole&language=en-us&strict=false&datasource=tranquility`)
