9dc2dfdecb6ac30ff35837774b377e9471d26a82
Robert Schuh <gitlab@eneticum.de>
2017-01-20 14:41:05 +0100

    clock

--- public/js/component/Clock.js
+++ public/js/component/Clock.js
@@ -3,8 +3,36 @@
 
 	class Clock extends Component {
 
+		constructor (props) {
+			super(props);
+			this.state = {
+				time: this.dateToStr(new Date())
+			}
+		}
+
+		componentDidMount () {
+			let d = new Date();
+			this.setState({ time: this.dateToStr(d) });
+			setTimeout(() =>
+				this.updateTime() || (this.interval = setInterval(() => this.updateTime(), 1000 * 60)),
+				(60 - d.getSeconds()) * 1000
+			);
+		}
+
+		componentWillUnmount () {
+			clearInterval(this.interval);
+		}
+
+		updateTime () {
+			this.setState({ time: this.dateToStr(new Date()) });
+		}
+
+		dateToStr (d) {
+			return (d.getUTCHours() < 10 ? "0" : "") + d.getUTCHours() + ":" + (d.getUTCMinutes() < 10 ? "0" : "") + d.getUTCMinutes();
+		}
+
 		render () {
-			return E("div", { id: "clock" });
+			return E("div", { id: "clock" }, this.state.time);
 		}
 
 	}
