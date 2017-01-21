e75b61d7b544c3d8d8a8fb3ea87b84ceff0d544c
Robert Schuh <gitlab@eneticum.de>
2017-01-21 02:59:16 +0100

    stars

--- public/js/App.js
+++ public/js/App.js
@@ -12,9 +12,50 @@
 			};
 		}
 
-		/*componentDidUpdate () {
-			this.setState({ isOpen: false });
-		}*/
+		componentDidMount () {
+			this.background = particleground(document.getElementById('particles-background'), {
+				dotColor: 'rgba(255, 255, 255, 0.5)',
+				lineColor: 'rgba(255, 255, 255, 0.05)',
+				minSpeedX: 0.075,
+				maxSpeedX: 0.15,
+				minSpeedY: 0.075,
+				maxSpeedY: 0.15,
+				density: 30000, // One particle every n pixels
+				curvedLines: false,
+				proximity: 20, // How close two dots need to be before they join
+				parallaxMultiplier: 20, // Lower the number is more extreme parallax
+				particleRadius: 2, // Dot size
+				parallax: false,
+			});
+			this.background.pause();
+			this.foreground = particleground(document.getElementById('particles-foreground'), {
+				dotColor: 'rgba(255, 255, 255, 1)',
+				lineColor: 'rgba(255, 255, 255, 0.05)',
+				minSpeedX: 0.3,
+				maxSpeedX: 0.6,
+				minSpeedY: 0.3,
+				maxSpeedY: 0.6,
+				density: 50000, // One particle every n pixels
+				curvedLines: false,
+				proximity: 250, // How close two dots need to be before they join
+				parallaxMultiplier: 10, // Lower the number is more extreme parallax
+				particleRadius: 4, // Dot size
+				parallax: false,
+			});
+			this.foreground.pause();
+			window.addEventListener("resize", this.resizeHandler.bind(this));
+		}
+
+		resizeHandler (e) {
+			if(this.background) {
+				this.background.start();
+				this.background.pause();
+			}
+			if(this.foreground) {
+				this.foreground.start();
+				this.foreground.pause();
+			}
+		}
 
 		searchBarHandler (e) {
 			switch (e.type) {
@@ -35,6 +76,8 @@
 
 		render () {
 			return E("div", { className: `ui ${this.state.isSearching ? "searching" : ""} ${this.state.isLoading ? "loading" : ""} ${this.state.isOpen ? "open" : "close"}` },
+				E("div", { id: "particles-background", className: "vertical-centered-box"}),
+				E("div", { id: "particles-foreground", className: "vertical-centered-box"}),
 				E(SideBarToggle, { sideBarToggleHandler: this.sideBarToggleHandler.bind(this) }),
 				E(SideBar, { isOpen: this.state.isOpen }),
 				E("div", { className: "content" },
