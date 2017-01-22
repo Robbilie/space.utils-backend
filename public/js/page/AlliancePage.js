1df897c67ad1f76df28414b9cfab049119931831
Robert Schuh <gitlab@eneticum.de>
2017-01-22 19:58:20 +0100

    some alli stuff

--- public/js/page/AlliancePage.js
+++ public/js/page/AlliancePage.js
@@ -3,8 +3,30 @@
 
 	class AlliancePage extends Component {
 
+		constructor (props) {
+			super(props);
+			this.load();
+		}
+
+		load () {
+			EASClient
+				.then(client => client.alliances.AllianceHandler_get_by_id({ alliance_id: this.props.id }))
+				.then(({ obj }) => this.setState(obj));
+		}
+
 		render () {
-			return E("div", { className: "page" });
+			return E("div", { className: "page" },
+				E("div", { className: "left-col" },
+					E("img", { src: `https://imageserver.eveonline.com/Alliance/${this.state.id}_128.png` }),
+					E("div", {},
+						E("div", { className: "row" }, "Ticker", this.state.ticker),
+						E("div", { className: "row" }, "Executor", this.state.executor_corporation.name)
+					)
+				),
+				E("div", { className: "right-col" },
+					E("h2", {}, "Alliance", this.state.name)
+				)
+			);
 		}
 
 	}
