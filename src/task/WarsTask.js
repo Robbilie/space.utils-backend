3494cd4aa301a41e3f01d073d416d46e7d68fec4
Robert Schuh <gitlab@eneticum.de>
2017-01-26 15:10:36 +0100

    reverse order

--- src/task/WarsTask.js
+++ src/task/WarsTask.js
@@ -18,6 +18,11 @@
 
 			let war = WarStore.findOne({}, { sort: { id: -1 } }, true);
 			let last_war_id = 1 + (await war.is_null() ? 0 : await war.get_id());
+
+			times.push(Date.now() - start);
+
+			console.log("wars last_war_id", last_war_id);
+
 			let last_expires = 0;
 			let more_pages = false;
 
@@ -25,11 +30,15 @@
 
 				let { obj, headers } = await client.Wars.get_wars({ max_war_id: last_war_id + 2000});
 
+				more_pages = obj.length == 2000;
+
+				if (more_pages)
+					last_war_id = obj[0];
+
 				let ids = obj.reverse();
-				last_war_id = Math.max(last_war_id, ...ids);
+				ids.reverse().forEach(id => process.nextTick(() => WarStore.find_or_create(id)));
+
 				last_expires = new Date(headers.expires).getTime();
-				ids.reverse().forEach(id => WarStore.find_or_create(id));
-				more_pages = ids.length == 2000;
 
 			} while (more_pages);
 
