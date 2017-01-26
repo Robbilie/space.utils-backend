26d5290ccb6bb67e8092d16401f93ca955cc4b88
Robert Schuh <gitlab@eneticum.de>
2017-01-26 17:08:44 +0100

    more compact

--- src/task/WarsTask.js
+++ src/task/WarsTask.js
@@ -9,21 +9,15 @@
 
 		async start () {
 
-			let start = Date.now();
-			let times = [];
-
 			let client = await ESIUtil.get_client();
-
-			times.push(Date.now() - start);
 
 			let war = WarStore.findOne({}, { sort: { id: -1 } }, true);
 			let last_war_id = 1 + (await war.is_null() ? 0 : await war.get_id());
 
-			times.push(Date.now() - start);
 
 			console.log("wars last_war_id", last_war_id);
 
-			let last_expires = 0;
+			let expires = 0;
 			let more_pages = false;
 
 			do {
@@ -32,25 +26,18 @@
 
 				more_pages = obj.length == 2000;
 
-				if (more_pages)
+				if (obj.length)
 					last_war_id = obj[0];
 
-				let ids = obj.reverse();
-				ids.reverse().forEach(id => process.nextTick(() => WarStore.find_or_create(id)));
+				obj.reverse().forEach(id => process.nextTick(() => WarStore.find_or_create(id)));
 
-				last_expires = new Date(headers.expires).getTime();
+				expires = new Date(headers.expires).getTime();
 
 			} while (more_pages);
 
-			times.push(Date.now() - start);
-
 			await this.update({
-				expires: last_expires
+				expires
 			});
-
-			times.push(Date.now() - start);
-
-			console.log("wars", ...times);
 
 		}
 
