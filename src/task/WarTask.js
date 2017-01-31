8914556a1e0e965253858c4d39c4657031d65dfc
Robert Schuh <gitlab@eneticum.de>
2017-01-31 02:34:22 +0100

    fetch allies

--- src/task/WarTask.js
+++ src/task/WarTask.js
@@ -9,16 +9,9 @@
 
 		async start () {
 
-			let start = Date.now();
-			let times = [];
-
 			let client = await ESIUtil.get_client();
 
-			times.push(Date.now() - start);
-
 			let { obj, headers } = await client.Wars.get_wars_war_id(this.get_data());
-
-			times.push(Date.now() - start);
 
 			let war = obj;
 				if(war.declared)
@@ -30,15 +23,13 @@
 				if(war.retracted)
 					war.retracted = new Date(war.retracted).getTime();
 
-			let { finished, aggressor, defender } = obj;
+			let { finished, aggressor, defender, allies = [] } = obj;
 
 			await this.get_store().update(
 				{ id: this.get_data().war_id },
 				{ $set: obj },
 				{ upsert: true, w: 0 }
 			);
-
-			times.push(Date.now() - start);
 
 			if (aggressor.corporation_id)
 				BaseTask.create_task("Corporation", { corporation_id: aggressor.corporation_id }, true);
@@ -50,7 +41,12 @@
 			if (defender.alliance_id)
 				BaseTask.create_task("Alliance", { alliance_id: defender.alliance_id }, true);
 
-			times.push(Date.now() - start);
+			allies.forEach(({ corporations_id, alliance_id }) => {
+				if (corporations_id)
+					BaseTask.create_task("Corporation", { corporations_id }, true);
+				if (alliance_id)
+					BaseTask.create_task("Alliance", { alliance_id }, true);
+			});
 
 			for (let page of new Array(1000).keys()) {
 				let { obj } = await client.Wars.get_wars_war_id_killmails({ war_id: this.get_data().war_id, page });
@@ -59,32 +55,12 @@
 					break;
 			}
 
-			/*
-			const storage = {
-				more_killmails: true,
-				page: 1
-			};
-
-			while (storage.more_killmails) {
-				let { obj } = await client.Wars.get_wars_war_id_killmails({ war_id: this.get_data().war_id, page: storage.page++ });
-				obj.forEach(({ killmail_id, killmail_hash }) => KillmailStore.find_or_create(killmail_id, killmail_hash));
-				if (obj.length < 2000)
-					storage.more_killmails = false;
-			}
-			*/
-
-			times.push(Date.now() - start);
-
 			if (finished && finished < Date.now())
 				await this.destroy();
 			else
 				await this.update({
 					expires: new Date(headers.expires).getTime()
 				});
-
-			times.push(Date.now() - start);
-
-			//console.log("character", ...times);
 
 		}
 
