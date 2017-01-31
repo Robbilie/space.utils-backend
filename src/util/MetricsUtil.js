c5b28a8a78351af40c35fe598ffe597664f46ebc
Robert Schuh <gitlab@eneticum.de>
2017-01-31 02:07:22 +0100

    does this work?

--- src/util/MetricsUtil.js
+++ src/util/MetricsUtil.js
@@ -22,7 +22,7 @@
 			storage.report = new Report();
 			storage.metrics.forEach((value, key) => storage.report.addMetric(key, value));
 
-			storage.reporter = new GraphiteReporter(storage.report, (`eas-kubes.pods.${os.hostname()}`).replace(/-/g, "_"), process.env.GRAPHITE_HOST);
+			storage.reporter = new GraphiteReporter(storage.report, (`eas-kubes.pods.${process.env.HOSTNAME}`).replace(/-/g, "_"), process.env.GRAPHITE_HOST);
 			storage.reporter.on("log", (level, msg, exc) => {
 				if(exc) {
 					console.log(`${level} -- ${msg} (${exc})`);
