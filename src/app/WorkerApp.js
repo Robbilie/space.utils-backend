43267d033a5d9bc986c2dd50b185672e21a456ed
Robert Schuh <gitlab@eneticum.de>
2017-02-02 11:46:10 +0100

    move duration

--- src/app/WorkerApp.js
+++ src/app/WorkerApp.js
@@ -189,8 +189,6 @@
 
 			this.heartbeat = Date.now();
 
-			let start = process.hrtime();
-
 			try {
 
 				let now = Date.now();
@@ -206,10 +204,13 @@
 
 				// do special processing stuff or error out
 				try {
+					let start = process.hrtime();
 					//console.log("start", JSON.stringify(value));
 					await new (LoadUtil.task(name))(value).start();
 					//console.log("end", JSON.stringify(value));
 					this.completed++;
+					let duration = process.hrtime(start);
+					MetricsUtil.get("tasks.duration").update((duration[0] * 1e9 + duration[1]) / 1e6);
 				} catch (e) {
 
 					await WorkerApp.get_tasks().update({ _id }, { $set: { "info.modified": Date.now() } });
@@ -233,10 +234,6 @@
 			}
 
 			MetricsUtil.get("tasks.completed").inc(1);
-
-			let duration = process.hrtime(start);
-
-			MetricsUtil.get("tasks.duration").update((duration[0] * 1e9 + duration[1]) / 1e6);
 
 			this.heartbeat = Date.now();
 
