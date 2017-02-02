1cb8b617dcfdbba3944c11c3ad2eb0a9b430c66e
Robert Schuh <gitlab@eneticum.de>
2017-02-02 02:07:39 +0100

    quick, add some stuff

--- public/js/page/AlliancePage.js
+++ public/js/page/AlliancePage.js
@@ -26,24 +26,37 @@
 		render () {
 			return E("div", { className: "page two-col-page" },
 				E("div", { className: "left-col" },
-					E("img", { src: `https://imageserver.eveonline.com/Alliance/${this.state.id}_128.png` }),
+					E("img", { style: { width: "256px" }, src: `https://imageserver.eveonline.com/Alliance/${this.state.id}_128.png` }),
+					E("h2", { className: "mobile" },
+						E("span", null, "Alliance"),
+						E("br"),
+						E("b", null, this.state.name)
+					),
 					E("div", { className: "info-list" },
 						E("div", { className: "" },
-							E("span", null, "Ticker"),
-							E("br"),
-							E("b", null, this.state.ticker)
+							E("div", { className: "" }),
+							E("div", { className: "" },
+								E("span", null, "Ticker"),
+								E("br"),
+								E("b", null, this.state.ticker)
+							)
 						),
 						E("div", { className: "" },
-							E("span", null, "Executor"),
-							E("br"),
-							E("b", null,
-								E(Link, { to: `/corporations/${this.state.executor_corporation.id}/` }, this.state.executor_corporation.name)
+							E("div", { className: "" },
+								E("img", { src: `https://imageserver.eveonline.com/Corporation/${this.state.executor_corporation.id}_32.png` })
+							),
+							E("div", { className: "" },
+								E("span", null, "Executor"),
+								E("br"),
+								E("b", null,
+									E(Link, { to: `/corporations/${this.state.executor_corporation.id}/` }, this.state.executor_corporation.name)
+								)
 							)
 						)
 					)
 				),
 				E("div", { className: "right-col" },
-					E("h2", {},
+					E("h2", { className: "desktop" },
 						E("span", null, "Alliance"),
 						E("br"),
 						E("b", null, this.state.name)
--- public/js/page/CorporationPage.js
+++ public/js/page/CorporationPage.js
@@ -26,31 +26,49 @@
 		render () {
 			return E("div", { className: "page two-col-page" },
 				E("div", { className: "left-col" },
-					E("img", { src: `https://imageserver.eveonline.com/Corporation/${this.state.id}_128.png` }),
+					E("img", { src: `https://imageserver.eveonline.com/Corporation/${this.state.id}_256.png` }),
+					E("h2", { className: "mobile" },
+						E("span", null, "Corporation"),
+						E("br"),
+						E("b", null, this.state.name)
+					),
 					E("div", { className: "info-list" },
 						E("div", { className: "" },
-							E("span", null, "Ticker"),
-							E("br"),
-							E("b", null, this.state.ticker)
+							E("div", { className: "" }),
+							E("div", { className: "" },
+								E("span", null, "Ticker"),
+								E("br"),
+								E("b", null, this.state.ticker)
+							)
 						),
 						E("div", { className: "" },
-							E("span", null, "CEO"),
-							E("br"),
-							E("b", null,
-								E(Link, { to: `/characters/${this.state.ceo.id}/` }, this.state.ceo.name)
+							E("div", { className: "" },
+								E("img", { src: `https://imageserver.eveonline.com/Character/${this.state.ceo.id}_32.jpg` })
+							),
+							E("div", { className: "" },
+								E("span", null, "CEO"),
+								E("br"),
+								E("b", null,
+									E(Link, { to: `/characters/${this.state.ceo.id}/` }, this.state.ceo.name)
+								)
 							)
 						),
 						this.state.alliance ? E("div", { className: "" },
-							E("span", null, "Alliance"),
-							E("br"),
-							E("b", null,
-								E(Link, { to: `/alliances/${this.state.alliance.id}/` }, this.state.alliance.name)
+							E("div", { className: "" },
+								E("img", { src: `https://imageserver.eveonline.com/Alliance/${this.state.alliance.id}_32.png` })
+							),
+							E("div", { className: "" },
+								E("span", null, "Alliance"),
+								E("br"),
+								E("b", null,
+									E(Link, { to: `/alliances/${this.state.alliance.id}/` }, this.state.alliance.name)
+								)
 							)
 						) : ""
 					)
 				),
 				E("div", { className: "right-col" },
-					E("h2", {},
+					E("h2", { className: "desktop" },
 						E("span", null, "Corporation"),
 						E("br"),
 						E("b", null, this.state.name)
