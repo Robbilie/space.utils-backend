ec5bd266c32fbadde83fa44cffcae73c1630ee8e
Robert Schuh <gitlab@eneticum.de>
2017-02-04 02:45:26 +0100

    dynamic base

--- src/app/WorkerApp.js
+++ src/app/WorkerApp.js
@@ -216,7 +216,9 @@
 					//console.log("end", JSON.stringify(value));
 					this.completed++;
 					let duration = process.hrtime(start);
-					MetricsUtil.update("tasks.duration", (duration[0] * 1e9 + duration[1]) / 1e6);
+						duration = (duration[0] * 1e9 + duration[1]) / 1e6;
+					MetricsUtil.update("tasks.duration", duration);
+					MetricsUtil.update(`tasks.type.${name}`);
 				} catch (e) {
 
 					await WorkerApp.get_tasks().update({ _id }, { $set: { "info.modified": Date.now() } });
