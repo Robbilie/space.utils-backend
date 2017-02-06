e4b1c95ddb789fa4af020e1ee035f7a106f84db0
Robert Schuh <gitlab@eneticum.de>
2017-02-06 16:54:32 +0100

    test

--- public/js/component/Page.js
+++ public/js/component/Page.js
@@ -3,4 +3,17 @@
 
 	class Page extends Component {
 
+		constructor (props) {
+			super(props);
+			this.done = () => {};
+		}
+
+		componentWillAppear (cb) {
+			this.done = cb;
+		}
+
+		componentWillEnter (cb) {
+			this.done = cb;
+		}
+
 	}
