98e1b70711777b2fe23d5834c776a717aee198b6
Robert Schuh <gitlab@eneticum.de>
2017-02-06 21:30:25 +0100

    hm how to get the child…

--- public/js/component/PageTransition.js
+++ public/js/component/PageTransition.js
@@ -3,28 +3,9 @@
 
 	class PageTransition extends React.addons.CSSTransitionGroup {
 
-		_customWrapChild (child) {
-			console.log("hello wrap child");
-			return React.createElement(
-				ReactCSSTransitionGroupChild,
-				{
-					name: this.props.transitionName,
-					appear: this.props.transitionAppear,
-					enter: this.props.transitionEnter,
-					leave: this.props.transitionLeave,
-					appearTimeout: this.props.transitionAppearTimeout,
-					enterTimeout: this.props.transitionEnterTimeout,
-					leaveTimeout: this.props.transitionLeaveTimeout,
-				},
-				child
-			);
-		}
-
-		render() {
-			return E(
-				React.addons.TransitionGroup,
-				Object.assign({}, this.props, { childFactory: this._customWrapChild })
-			);
+		constructor (props) {
+			super(props);
+			window.transition = this;
 		}
 
 	}
