0f98ab56c4829e16f726a3297cb656bdb63357b1
Robert Schuh <gitlab@eneticum.de>
2017-02-06 20:50:19 +0100

    does that trigger?

--- public/js/component/PageTransition.js
+++ public/js/component/PageTransition.js
@@ -3,4 +3,21 @@
 
 	class PageTransition extends React.addons.CSSTransitionGroup {
 
+		_wrapChild (child) {
+			console.log("hello wrap child");
+			return React.createElement(
+				ReactCSSTransitionGroupChild,
+				{
+					name: this.props.transitionName,
+					appear: this.props.transitionAppear,
+					enter: this.props.transitionEnter,
+					leave: this.props.transitionLeave,
+					appearTimeout: this.props.transitionAppearTimeout,
+					enterTimeout: this.props.transitionEnterTimeout,
+					leaveTimeout: this.props.transitionLeaveTimeout,
+				},
+				child
+			);
+		}
+
 	}
