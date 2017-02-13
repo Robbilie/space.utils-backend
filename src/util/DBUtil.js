8faf71eebc4c86c97209dbc2724efddcdf35f3c4
Robert Schuh <gitlab@eneticum.de>
2017-02-13 18:28:09 +0100

    disable logs

--- src/util/DBUtil.js
+++ src/util/DBUtil.js
@@ -96,7 +96,7 @@
 
 		static oplog ({ op, ns, ts = Timestamp(0, Date.now() / 1000 | 0), o, o2 } = {}) {
 			DBUtil.get_oplog().then(oplog => {
-				console.log(JSON.stringify({ op, ns, ts, o, o2 }));
+				//console.log(JSON.stringify({ op, ns, ts, o, o2 }));
 				oplog.collection("oplog").insertOne({ op, ns, ts, o: op == "u" ? DBUtil.strip(o) : o, o2: op == "u" ? DBUtil.strip(o2) : o2 });
 			});
 		}
