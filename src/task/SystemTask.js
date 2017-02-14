e728ff0f415a4ff138a1c1b480142b0480ae0e6e
Robert Schuh <gitlab@eneticum.de>
2017-02-14 21:48:06 +0100

    tweaks

--- src/task/SystemTask.js
+++ src/task/SystemTask.js
@@ -13,9 +13,8 @@
 			let { obj: system } = await client.Universe.get_universe_systems_system_id(this.get_data());
 
 			system = Object.assign(system, {
-				id: 				this.get_data().system_id,
-				name: 				system.solar_system_name,
-				solar_system_name: 	undefined
+				id: 			system.system_id,
+				system_id: 		undefined
 			});
 
 			await this.get_store().insert(system);
