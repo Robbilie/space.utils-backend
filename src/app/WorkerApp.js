bb54d8027c8870040f5b1ca5477c5a7e400a4329
Robert Schuh <gitlab@eneticum.de>
2017-02-18 00:12:14 +0100

    some back off maybe?

--- src/app/WorkerApp.js
+++ src/app/WorkerApp.js
@@ -113,7 +113,7 @@
 				const run = (runnable, done) => {
 					this.process_next(runnable)
 						.catch(e => console.log("shouldn't happen", e) || true)
-						.then(should_wait => should_wait ? Promise.resolve().wait(200) : Promise.resolve())
+						.then(should_wait => should_wait ? (console.log("back off") || Promise.resolve().wait(500)) : Promise.resolve())
 						.then(() => {
 							if(this.queued_tasks.length)
 								this.queued_tasks.shift()();
