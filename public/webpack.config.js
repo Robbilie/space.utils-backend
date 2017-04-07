05f1193c8f3f7ab23bf15704efffc386b492f713
Robert Schuh <gitlab@eneticum.de>
2017-04-07 13:12:04 +0200

    will all be reverted

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,25 +1,64 @@
-const path = require("path");
-const webpack = require("webpack");
+const webpack = require('webpack');
+const path = require('path');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
 
 module.exports = {
-    entry: './js/app.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
+	devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+	context: path.join(__dirname, './client'),
+	entry: {
+		jsx: './js/init.js',
+		html: './index.html',
+		vendor: ['react']
+	},
+	output: {
+		path: path.join(__dirname, './build'),
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.html$/,
+				loader: 'file?name=[name].[ext]'
+			},
+			{
+				test: /\.css$/,
+				loaders: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loaders: [
+					'react-hot',
+					'babel-loader'
+				]
+			},
+		],
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+		root: [
+			path.resolve('./client')
+		]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			},
+			sourceMap: false
+		}),
+		new webpack.DefinePlugin({
+			'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+		})
+	],
+	devServer: {
+		contentBase: './client',
+		hot: true
+	}
 };
