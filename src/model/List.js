e1874a7051114cd14ed693edb46ce28860a7c731
Robert Schuh <gitlab@eneticum.de>
2017-04-23 02:49:51 +0200

    grah

--- src/model/List.js
+++ src/model/List.js
@@ -9,7 +9,7 @@
 		constructor (type, future_data) {
 			super(future_data.then(data => data.map(doc => Store.from_data(doc, type))));
 			if (this.constructor.name == "KillmailList")
-				console.log("kml", future_data);
+				future_data.then(d => console.log("kml", d));
 			this.type = type;
 		}
 
