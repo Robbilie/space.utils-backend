2bb8b42586cd90842d1c425c746e32360fd1c28a
Robert Schuh <gitlab@eneticum.de>
2017-04-23 03:00:18 +0200

    grah

--- src/handler/EntityHandler.js
+++ src/handler/EntityHandler.js
@@ -10,9 +10,9 @@
 			console.log("get filter", this.name);
 			res.json({ items: await LoadUtil
 				.store(`${this.name.slice(0, -7)}`)
-				.from_cursor(c => c
+				//.from_cursor(c => c
 					.find(InputUtil.sanitize(filter), InputUtil.limit(options))
-				)
+				//)
 				.serialize() });
 		}
 
