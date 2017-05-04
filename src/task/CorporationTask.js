cd2439d7b5fe1f089da7d9b8b5d7b25839794e03
Robert Schuh <gitlab@eneticum.de>
2017-05-04 18:28:46 +0200

    use replace

--- src/task/CorporationTask.js
+++ src/task/CorporationTask.js
@@ -3,61 +3,51 @@
 
 	const { BaseTask } = require("task/");
 	const { ESIUtil } = require("util/");
-	const { CharacterStore, CorporationStore, AllianceStore } = require("store/");
+	const { CorporationStore } = require("store/");
 
 	class CorporationTask extends BaseTask {
 
 		async start () {
 
-			let tss = [Date.now()];
-
-			let client = await ESIUtil.get_client();
-
-			tss.push(Date.now());
+			const client = await ESIUtil.get_client();
 
 			let [{ body: corporation, headers }, old_corp] = await Promise.all([
 				client.apis.Corporation.get_corporations_corporation_id(this.get_data()),
 				CorporationStore.find_by_id(this.get_data().corporation_id).get_future()
 			]);
 
-			tss.push(Date.now());
-
 			let alliance_history = null;
-			if (!old_corp || (old_corp && old_corp.alliance_id != corporation.alliance_id)) {
+			if (!old_corp || (old_corp && old_corp.alliance_id !== corporation.alliance_id)) {
 				let { body: history } = await client.apis.Corporation.get_corporations_corporation_id_alliancehistory(this.get_data());
-				alliance_history = history;
+				alliance_history = history.map(entry => Object.assign(entry, { start_date: new Date(entry.start_date).getTime() }));
+			} else {
+				alliance_history = old_corp.alliance_history;
 			}
 
-			tss.push(Date.now());
-
-			corporation = Object.assign(corporation, {
-				id: 				this.get_data().corporation_id,
-				name: 				corporation.name || corporation.corporation_name,
-				corporation_name: 	undefined
-			}, alliance_history ? {
-				alliance_history:	alliance_history.map(entry => Object.assign(entry, { start_date: new Date(entry.start_date).getTime() }))
-			} : {});
-
-			tss.push(Date.now());
+			let doc = Object.assign(
+				{},
+				corporation,
+				{
+					id: 				this.get_data().corporation_id,
+					name: 				corporation.name || corporation.corporation_name,
+					creation_date: 		new Date(corporation.creation_date).getTime(),
+					alliance_history
+				}
+			);
+			// TODO : ESI should fix this
+			delete doc.corporation_name;
 
 			let { ceo_id, alliance_id, member_count } = corporation;
 
-			await this.get_store().update(
-				{ id: corporation.id },
-				{
-					$set: corporation,
-					$unset: { [alliance_id ? "unset" : "alliance_id"]: true }
-				},
+			await this.get_store().replace(
+				{ id: doc.id },
+				doc,
 				{ upsert: true }
 			);
-
-			tss.push(Date.now());
 
 			// get alliance
 			if(alliance_id)
 				this.enqueue_reference("Alliance", alliance_id);
-
-			tss.push(Date.now());
 
 			// get all alliances
 			if (alliance_history)
@@ -65,10 +55,8 @@
 					.filter(({ alliance }) => !!alliance)
 					.map(({ alliance: { alliance_id } }) => this.enqueue_reference("Alliance", alliance_id));
 
-			tss.push(Date.now());
-
 			// get ceo
-			if(ceo_id == 1) {
+			if(ceo_id === 1) {
 				// dead corp
 			} else if(ceo_id >= 3000000 && ceo_id < 4000000) {
 				// is npc ceo
@@ -78,16 +66,10 @@
 				console.log("no ceo", this.get_data().corporation_id);
 			}
 
-			tss.push(Date.now());
-
-			if(ceo_id == 1 || member_count == 0)
+			if(ceo_id === 1 || member_count === 0)
 				await this.destroy();
 			else
 				await this.update({ expires: new Date(headers.expires).getTime() });
-
-			tss.push(Date.now());
-
-			//console.log("corporation", ...tss.map((t, i, a) => t - (a[i - 1] || t)));
 
 		}
 
