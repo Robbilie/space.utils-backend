55e76a95dd12f6f63a03e5e239e6dd02251bd93f
Robert Schuh <gitlab@eneticum.de>
2017-05-08 22:28:35 +0200

    corporations

--- src/task/AllianceTask.js
+++ src/task/AllianceTask.js
@@ -11,7 +11,7 @@
 
 			const client = await ESIUtil.get_client();
 
-			let [{ body: alliance, headers }, { body: corporations }] = await Promise.all([
+			let [{ body: alliance, headers }, { body: corporation_ids }] = await Promise.all([
 				client.apis.Alliance.get_alliances_alliance_id(this.get_data()),
 				client.apis.Alliance.get_alliances_alliance_id_corporations(this.get_data())
 			]);
@@ -19,7 +19,8 @@
 			alliance = Object.assign(alliance, {
 				id: 						this.get_data().alliance_id,
 				name:						alliance.name || alliance.alliance_name,
-				date_founded: 				new Date(alliance.date_founded).getTime()
+				date_founded: 				new Date(alliance.date_founded).getTime(),
+				corporation_ids
 			});
 
 			// manage optional convert
@@ -41,16 +42,16 @@
 				);
 
 				const alliance_id = alliance.id;
-				const corporation_ids = await CorporationStore.from_cursor(c => c.find({ alliance_id }).project({ id: 1 })).map(corporation => corporation.get_id());
+				const old_corporation_ids = await CorporationStore.from_cursor(c => c.find({ alliance_id }).project({ id: 1 })).map(corporation => corporation.get_id());
 
-				corporations
-					.filter(corporation_id => !corporation_ids.includes(corporation_id))
+				corporation_ids
+					.filter(corporation_id => !old_corporation_ids.includes(corporation_id))
 					.forEach(corporation_id => this.enqueue_reference("Corporation", corporation_id));
 
 			}
 
 			await this.update({
-				expires: (corporations.length === 0) ? Number.MAX_SAFE_INTEGER : new Date(headers.expires).getTime(),
+				expires: (corporation_ids.length === 0) ? Number.MAX_SAFE_INTEGER : new Date(headers.expires).getTime(),
 				hash
 			});
 
