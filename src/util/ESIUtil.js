3e027aecf9acf14075002297b08d221afa451bf4
Robert Schuh <gitlab@eneticum.de>
2017-05-08 23:19:34 +0200

    disable gzip

--- src/util/ESIUtil.js
+++ src/util/ESIUtil.js
@@ -5,7 +5,7 @@
 	const Swagger 	= require("swagger-client");
 	const rp 		= require("request-promise-native");
 	const request 	= rp.defaults({
-		gzip: true, // actually slows down
+		// gzip: true, // actually slows down
 		forever: true,
 		//timeout: 1000 * 12,
 		pool: {
