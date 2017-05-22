5eda930c0ba7b71af54daa32857fd362d03d0a6d
Robert Schuh <gitlab@eneticum.de>
2017-05-23 00:17:23 +0200

    hmmm…

--- src/model/List.js
+++ src/model/List.js
@@ -7,11 +7,14 @@
 	class List extends Base {
 
 		constructor (type, future_data) {
-			super(future_data.then(data => data.map(doc => Store.from_data(doc, type))));
-			this.raw = future_data;
-			//if (this.constructor.name == "KillmailList")
-			//	future_data.then(d => console.log("kml", d));
+			super(future_data);
 			this.type = type;
+		}
+
+		get_future () {
+			if (this.list === undefined)
+				this.list = this.get_raw().then(data => data.map(doc => Store.from_data(doc, type)));
+			return this.list;
 		}
 
 		get_type () {
@@ -26,11 +29,12 @@
 			return this.get_future();//.then(arr => arr.map(el => new (this.get_type())(el)));
 		}
 
-		map (fn) {
-			return this.get_future().then(data => Promise.all(data.map(fn)));
+		map (fn, bare = false) {
+			return this[bare === true ? "get_raw" : "get_future"]().then(data => Promise.all(data.map(fn)));
 		}
 
 		async serialize (depth = 2) {
+			return await this.get_raw();
 			let data = await this.get_future();
 			return Promise.all(data.map(element => element.serialize(depth - 1)));
 		}
