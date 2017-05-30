efd158804d80e1e8c5517d6e8478fcb7378bebec
Robert Schuh <gitlab@eneticum.de>
2017-05-30 02:50:41 +0200

    cache proxy wrap

--- src/util/ProxyWrap.js
+++ src/util/ProxyWrap.js
@@ -1,10 +1,14 @@
 
 	"use strict";
 
+	const cache = new WeakMap();
+
 	module.exports = function ProxyWrap (p) {
-		return new Proxy(p, {
-			get: (promise, name) => name === "then" ?
-				(...args) => promise.then(...args) :
-				(...args) => ProxyWrap(promise.then(object => object[name](...args)))
-		});
+		if (cache.has(p) === false)
+			cache.set(p, new Proxy(p, {
+				get: (promise, name) => name === "then" ?
+					(...args) => promise.then(...args) :
+					(...args) => ProxyWrap(promise.then(object => object[name](...args)))
+			}));
+		return cache.get(p);
 	};
