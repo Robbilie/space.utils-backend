0cdd2a99cc9b584a5bfd232009c4f37a07e7d2bb
Robert Schuh <gitlab@eneticum.de>
2017-08-19 22:26:51 +0200

    debugging the zkbhistory task

--- src/task/ZKBHistoryTask.js
+++ src/task/ZKBHistoryTask.js
@@ -26,17 +26,21 @@
 
 			const date = new Date(page);
 
-			if (start === false)
-				await this.tick({ page });
+			{
+				if (start === false)
+					await this.tick({ page });
 
-			const res = await request(`https://zkillboard.com/api/history/${this.get_url_date(date)}/`);
+				const res = await request(`https://zkillboard.com/api/history/${this.get_url_date(date)}/`);
 
-			await Promise.all(Object
-				.entries(res)
-				.map(([killmail_id, killmail_hash]) => [parseInt(killmail_id), killmail_hash])
-				.sort(([killmail_id_a], [killmail_id_b]) => killmail_id_a > killmail_id_b ? 1 : -1)
-				.filter(([killmail_id, killmail_hash]) => killmail_hash.length === 40)
-				.map(([killmail_id, killmail_hash]) => BaseTask.create_task("Killmail", { killmail_id, killmail_hash })));
+				console.log(res);
+
+				await Promise.all(Object
+					.entries(res)
+					.map(([killmail_id, killmail_hash]) => [parseInt(killmail_id), killmail_hash])
+					.sort(([killmail_id_a], [killmail_id_b]) => killmail_id_a > killmail_id_b ? 1 : -1)
+					.filter(([killmail_id, killmail_hash]) => killmail_hash.length === 40)
+					.map(([killmail_id, killmail_hash]) => BaseTask.create_task("Killmail", { killmail_id, killmail_hash })));
+			}
 
 			if (this.get_url_date(date) !== this.get_url_date())
 				return await this.get_pages(this.get_url_date(!date.setDate(date.getDate() + 1) || date, "-"));
