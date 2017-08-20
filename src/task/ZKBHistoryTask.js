c9951695c5783a7a1772a7658d36a7b54a3dec01
Robert Schuh <gitlab@eneticum.de>
2017-08-20 18:34:31 +0200

    logs

--- src/task/ZKBHistoryTask.js
+++ src/task/ZKBHistoryTask.js
@@ -29,14 +29,17 @@
 
 				const res = await request(`https://zkillboard.com/api/history/${this.get_url_date(date)}/`);
 
+				const start = Date.now();
 				//await Promise.all(
 					Object
 						.entries(res)
 						.map(([killmail_id, killmail_hash]) => [parseInt(killmail_id), killmail_hash])
 						.sort(([killmail_id_a], [killmail_id_b]) => killmail_id_a > killmail_id_b ? 1 : -1)
 						.filter(([killmail_id, killmail_hash]) => killmail_hash.length === 40)
-						.map(([killmail_id, killmail_hash]) => BaseTask.create_task("Killmail", { killmail_id, killmail_hash }))
+						//.map(([killmail_id, killmail_hash]) => BaseTask.create_task("Killmail", { killmail_id, killmail_hash }))
+						.map(([killmail_id, killmail_hash]) => this.enqueue_reference("Killmail", killmail_id, killmail_hash))
 				//);
+				console.log(page, "took", (Date.now() - start) / 1000, "seconds");
 			}
 
 			if (this.get_url_date(date) !== this.get_url_date())
