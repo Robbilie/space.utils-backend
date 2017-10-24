10df7d3bdd521bd6d6d69e8a55b2573c16211822
Robert Schuh <git@eneticum.de>
2017-10-25 01:37:06 +0200

    debugggggggg

--- src/util/PromiseWrap.js
+++ src/util/PromiseWrap.js
@@ -4,7 +4,6 @@
 	const cache = new WeakMap();
 
 	function PromiseWrap (p) {
-		/*
 		if (cache.has(p) === false)
 			cache.set(p, new Proxy(p, {
 				get: (promise, name) => name === "then" ?
@@ -12,12 +11,6 @@
 					(...args) => PromiseWrap(promise.then(object => object[name](...args)))
 			}));
 		return cache.get(p);
-		*/
-		return new Proxy(p, {
-			get: (promise, name) => name === "then" ?
-				(...args) => promise.then(...args) :
-				(...args) => PromiseWrap(promise.then(object => object[name](...args)))
-		});
 	}
 
 	module.exports = PromiseWrap;
